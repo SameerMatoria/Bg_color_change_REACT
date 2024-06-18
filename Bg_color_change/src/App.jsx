43eea5f1c790a5ad83657e1afd8e3e04579f8ae1
SameerMatoria <sameermatoria@gmail.com>
2024-06-18 23:28:47 +0530

    nearly done

--- Bg_color_change/src/App.jsx
+++ Bg_color_change/src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="" 
+      style={{
+        width:"100vw",
+        height:"90vh",
+        backgroundColor: color,
+        color:"black",
+        borderRadius: "10px",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        gap:"20px",
+        margin:"-100px"
+        }}>
+          <button onClick={()=>setColor("red")}
+          style={{
+            height:"70px",
+            width:"100px",
+            }}>Red</button>
+          <button onClick={()=>setColor("green")}
+          style={{
+            height:"70px",
+            width:"100px",
+            }}>Green</button> 
+          <button onClick={()=>setColor("blue")}
+           style={{
+            height:"70px",
+            width:"100px",
+            }}>Blue</button>  
+          </div>  
     </>
   )
 }
